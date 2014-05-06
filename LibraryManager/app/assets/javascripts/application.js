63a7df9f450c7a4b14e2686ede33b96bfcabf04c
unknown <nikita singh@FIR000088994008.ads.mckinsey.com>
2014-05-06 17:44:38 +0530

    width changed to 800px

--- LibraryManager/app/assets/javascripts/application.js
+++ LibraryManager/app/assets/javascripts/application.js
@@ -58,7 +58,6 @@ function Randomize() {
 	var images = new Array("/assets/1.jpg",
 						   "/assets/2.jpg",
 						   "/assets/3.jpg",
-						   "/assets/6.jpg",
 						   "/assets/7.jpg",
 						   "/assets/8.jpg",
 						   "/assets/9.jpg",
